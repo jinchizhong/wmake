4162107480dab91d88f0b77f37b2c3b86f0a005c
jinchizhong <jinchizhong@kingsoft.com>
2014-06-11 06:09:47 +0800

    complete load

--- lib/wmake/front.rb
+++ lib/wmake/front.rb
@@ -1,10 +1,21 @@
 module WMake
   class Front
+    def initialize
+      @loading_stack = []
+    end
+    attr_reader :loading_stack
     def load fpath
+      @loading_stack << fpath
       instance_eval File.read(fpath), fpath
+      @loading_stack.pop
     end
     def project name, type, &block
       WMake::Project.new name, type, &block
+    end
+    def configure
+      WMake::PROJECTS.each_value do |proj|
+        proj.configure
+      end
     end
   end
   FRONT = Front.new
--- lib/wmake/project.rb
+++ lib/wmake/project.rb
@@ -4,11 +4,19 @@ module WMake
     def initialize name, type = :exec, &block
       WMake.die "Project named \"#{name}\" already exists!" if WMake::PROJECTS[name]
       WMake.die "Unknown project type: #{type}" unless [:exec].include? type
-      WMake.die "Project no config!" unless block
+      WMake.die "Project without config!" unless block
       PROJECTS[name] = self
       @name = name
       @type = type
       @block = block
+      @wmake = WMake::FRONT.loading_stack.last
+      @files = []
+    end
+    def configure
+      instance_eval &@block
+    end
+    def add *args
+      @files += args
     end
   end
 end
